aeeb3ae30b2850ed2ce2c0cae94682bb7f1f31b7
Shrutika-kamble <167451093+Shrutika-kamble@users.noreply.github.com>
2025-12-05 12:50:39 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      { test: /\.jsx?$/, exclude: /node_modules/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+    ]
+  },
+  resolve: { extensions: ['.js', '.jsx'] },
+  devServer: { static: './', port: 3000 }
+};
